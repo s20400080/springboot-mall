ad297782c22a27da5f28f3fe058a862c10d96d78
s20400080 <s20400080@gms.ndhu.edu.tw>
2022-07-26 23:16:19 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/s202400080/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s202400080.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
