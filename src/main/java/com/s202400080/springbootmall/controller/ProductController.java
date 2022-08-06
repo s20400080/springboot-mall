2c659e42895464ca0cb11fda87f3263990e43215
s20400080 <s20400080@gms.ndhu.edu.tw>
2022-08-06 17:38:14 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總筆數)

--- src/main/java/com/s202400080/springbootmall/controller/ProductController.java
+++ src/main/java/com/s202400080/springbootmall/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.s202400080.springbootmall.dto.ProductQueryParams;
 import com.s202400080.springbootmall.dto.ProductRequest;
 import com.s202400080.springbootmall.model.Product;
 import com.s202400080.springbootmall.service.ProductService;
+import com.s202400080.springbootmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             //查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -46,9 +47,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得 product List
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        //取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        //分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
